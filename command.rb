b74e9c580b7eaafd7c2352f7a9f1ec02084883d8
xrobesx <xrobes@gmail.com>
2018-02-09 16:30:11 -0500

    - wink

--- command.rb
+++ command.rb
@@ -3,8 +3,8 @@ Dir['./coin_config/*.rb'].each {|file| require file }
 require './bitcoin_client_extensions.rb'
 class Command
   attr_accessor :result, :action, :user_name, :icon_emoji
-  ACTIONS = %w(balance deposit tip withdraw commands help disclaimer forum github site invite tipping links network help_balance help_deposit help_tip help_withdraw help_commands help_help help_forum help_github help_site help_invite blocks connections supply hashrate stakeweight help_blocks help_connections help_stakeweight help_supply help_hashrate help_tipping help_links help_network lets_smoke study logos love_you :wink:)
-  def initialize(slack_params)
+  ACTIONS = %w(balance deposit tip withdraw commands help disclaimer forum github site invite tipping links network help_balance help_deposit help_tip help_withdraw help_commands help_help help_forum help_github help_site help_invite blocks connections supply hashrate stakeweight help_blocks help_connections help_stakeweight help_supply help_hashrate help_tipping help_links help_network lets_smoke study logos love_you)
+    def initialize(slack_params)
     @coin_config_module = Kernel.const_get ENV['COIN'].capitalize
     text = slack_params['text']
     @params = text.split(/\s+/)
@@ -225,9 +225,6 @@ class Command
     @result[:icon_emoji] = @coin_config_module::LOVE_EMOJI
   end
   
-  def :wink:
-    @result[:text] = ":wink:"
-  end
 
 
 
