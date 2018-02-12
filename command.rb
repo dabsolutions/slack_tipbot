fcda6db8765455dcc85e4650666b22062788054d
xrobesx <xrobes@gmail.com>
2018-02-12 13:39:44 -0500

    test

--- command.rb
+++ command.rb
@@ -205,8 +205,7 @@ class Command
   
   #marijuanacoin commands
   def mar
-    word = $1
-    
+    word = explorer
     if word = site
     	@result[:text] = "Here you go, this is the MAR website."
     	@result[:attachments] = [{
