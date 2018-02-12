f171e090fe13eb0c541f8c2e38333f10795dbe86
xrobesx <xrobes@gmail.com>
2018-02-12 10:23:29 -0500

    test syntax

--- command.rb
+++ command.rb
@@ -205,7 +205,7 @@ class Command
   
   #marijuanacoin commands
   def mar
-    word = gets
+    word = 'site'
     
     if word = `site`
     @result[:text] = "Here you go, this is the MAR website."
