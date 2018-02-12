ada4dbf457cc5dd2ad2077a8191257e8efc2367a
xrobesx <xrobes@gmail.com>
2018-02-12 10:31:54 -0500

    ..

--- command.rb
+++ command.rb
@@ -205,7 +205,7 @@ class Command
   
   #marijuanacoin commands
   def mar
-    word = 'site'
+    word = 'site test'
     
     if word = `site`
     @result[:text] = "Here you go, this is the MAR website."
